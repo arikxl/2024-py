e727b0b3cfd256a68aaac48c25011f415341d72f
Arik Alexandrov <arikxl@gmail.com>
2024-01-10 23:03:15 +0200

    func

---
+++ functions.py
@@ -0,0 +1,14 @@
+def greeting():
+    print('good morning')
+    print('good morning')
+    print()
+
+# greeting()
+# greeting()
+
+def create_name(name, family):
+    first = name.capitalize()
+    last = family.capitalize()
+    print(first + ' ' + last)
+
+create_name('arik', 'alexandrov')
